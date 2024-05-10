1d3b3fcbbe8ac7394fb040a6ed853f1d0c9690f4
QueenVicki <Jokilolo9@gmail.com>
2024-05-10 16:44:16 +0100

    Updated scoring system and randomising questions

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,26 +1,44 @@
-let questions = [["Q1", "C1", "C2", "C3", "C4"],
-                ["Q2", "C1", "C2", "C3", "C4",],
-                ["Q3", "C1", "C2", "C3", "C4",],
-                ["Q4", "C1", "C2",]];
+let questions = [["Q1",1, "C1", "C2", "C3", "C4",],
+                ["Q2", 3,"C1", "C2", "C3", "C4",],
+                ["Q3",4, "C1", "C2", "C3", "C4",],
+                ["Q4",1, "C1", "C2",]];
+
+let randNum =0;
+let score =0;
+
 function displayQuestion(){
 let randNum = Math.floor(Math.random() * questions.length);
 console.log(randNum);
 
+console.log(questions[randNum][1]);
+
 document.getElementById("questions").innerHTML = questions[randNum][0];
-document.getElementById("choice1").innerHTML = questions[randNum][1];
-document.getElementById("choice2").innerHTML = questions[randNum][2];
-document.getElementById("choice3").innerHTML = questions[randNum][3];
-document.getElementById("choice4").innerHTML = questions[randNum][4];
+
+document.getElementById("choice1").innerHTML = questions[randNum][2];
+document.getElementById("choice2").innerHTML = questions[randNum][3];
+// document.getElementById("choice3").innerHTML = questions[randNum][4];
+// document.getElementById("choice4").innerHTML = questions[randNum][5];
 
  /* Check 3rd choice exists and display it */
- if (typeof questions[randNum][3] !== "undefined") {
-    document.getElementById("choice3").innerHTML = questions[randNum][3];
+ if (typeof questions[randNum][4] !== "undefined") {
+    document.getElementById("choice3").innerHTML = questions[randNum][4];
   } else document.getElementById("choice3").innerHTML = "";
 
   /* Check 4th choice exists and display it */
- if (typeof questions[randNum][4] !== "undefined") {
-    document.getElementById("choice4").innerHTML = questions[randNum][4];
+ if (typeof questions[randNum][5] !== "undefined") {
+    document.getElementById("choice4").innerHTML = questions[randNum][5];
   } else document.getElementById("choice4").innerHTML = "";
 }
 
-displayQuestion();
+function checkAnswer(usersChoice) {
+
+  if (usersChoice == questions[randNum][1]) {
+    score++;
+  } else score --;
+
+    document.getElementById("score").innerHTML = score;
+    displayQuestion();
+}
+
+/---------Main Processing starts here -------------------------*/
+displayQuestion(); 
